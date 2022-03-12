840ccb6b4de3bd1643076cfff2a3cd799c01b589
canerkesim <78441354+canerkesim@users.noreply.github.com>
2022-03-12 19:19:02 +0300

    Addition method
    I used the void method type to addition

---
+++ src/Callculator.java
@@ -0,0 +1,17 @@
+
+public class Callculator {
+
+	public static void main(String[] args) {
+
+		
+		  	
+	}
+
+	public void add(int a,int b) {
+		
+		
+		System.out.println("The result is "+(a+b));
+
+	}
+}
+
